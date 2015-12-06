2fbd140bf314202850e6f35dea6580e874aa774b
rebecasouza <rebeca_souza85@hotmail.com>
2015-12-06 16:24:20 -0300

    adding root and callback routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,12 @@
 Rails.application.routes.draw do
   resources :events
-  devise_for :users
+	
+	root 'events#index'
+	
+	match '/profile/:id/finish_signup' => 'users#finish_signup', via: [:get, :patch], :as => :finish_signup
+  
+	devise_for :users, :controllers => { omniauth_callbacks: 'omniauth_callbacks' }
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
