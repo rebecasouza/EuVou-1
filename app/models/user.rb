76ccbc4dc67480ba0fe74a9dff210d35828ccf1d
rebecasouza <rebeca_souza85@hotmail.com>
2015-12-06 04:10:31 -0300

    adding authority to user

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,25 @@
 class User < ActiveRecord::Base
   rolify
+	
+	include Authority::UserAbilities
+	
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+	
+	after_create :set_default_role, if: Proc.new { User.count > 1 }
+	
+	
+	
+	def email_verified?
+    self.email && self.email !~ TEMP_EMAIL_REGEX
+  end
+
+  private
+
+  def set_default_role
+    add_role :user
+  end
+	
 end
