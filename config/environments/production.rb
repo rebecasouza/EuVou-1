dff04b4f9c5d0cec57e9bb52464e646be248e184
rebecasouza <rebeca_souza85@hotmail.com>
2016-02-15 08:58:15 -0300

    going back on smtp

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -79,7 +79,7 @@ Rails.application.configure do
   config.active_record.dump_schema_after_migration = false
 	
 	# ActionMailer Config
-	config.action_mailer.default_url_options = { :host => 'euvou-test.herokuapp.com', protocol: 'http' }
+	config.action_mailer.default_url_options = { :host => 'euvou-test.herokuapp.com' }
 	config.action_mailer.delivery_method = :smtp
 	config.action_mailer.perform_deliveries = true
 	config.action_mailer.raise_delivery_errors = true
