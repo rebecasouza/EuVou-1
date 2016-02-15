bb83e7db2bccf2a23501b7a4547d19a2e8db8ab9
rebecasouza <rebeca_souza85@hotmail.com>
2016-02-15 09:23:31 -0300

    smtp atempt

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -88,7 +88,7 @@ Rails.application.configure do
 		address: "smtp.gmail.com",
 		port: 587,
 		domain: "gmail.com",
-		authentication: "plain",
+		authentication: "login",
 		enable_starttls_auto: true,
 		user_name: 'euvou.pds@gmail.com',
 		password: 'euvou2016'
