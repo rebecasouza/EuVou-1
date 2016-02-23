532784337283b9eb57907ce3083df88c0bc85653
nayarocha <nayaarocha@gmail.com>
2016-02-23 13:09:33 -0300

    alteracoes

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -107,7 +107,7 @@ class EventsController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def event_params
-			params.require(:event).permit(:title, :category_id, :description, :date, :image, :user_id, :street, :number, :city, :zip_code, :country, :local, :lat, :lon)
+			params.require(:event).permit(:title, :category_id, :description, :date, :image, :user_id, :street, :number, :city, :zip_code, :country, :location, :lat, :lon)
     end
     
 end
