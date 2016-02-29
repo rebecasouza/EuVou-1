3c067f7b0a633fd132fad358b1efe5c422867c80
rebecasouza <rebeca_souza85@hotmail.com>
2016-02-29 07:18:33 -0300

    adding euvou to events

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -9,7 +9,8 @@ class EventsController < ApplicationController
   def index
 		@events = Event.search(params[:search]).order(created_at: :desc)
     @categories = Category.order(name: :asc)
-
+  	@events_upcoming = Event.upcoming
+    @events_past = Event.past
     respond_to do |format|
       format.html # user_show.html.erb
       format.json {render json: @events.as_json}
@@ -88,7 +89,7 @@ class EventsController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def event_params
-			params.require(:event).permit(:title, :category_id, :description, :date, :image, :user_id, :local,  :location, address_attributes: [:id, :place, :location, :lat, :lon])
+			params.require(:event).permit(:title, :category_id, :description, :date, :image, :image_cache, :user_id, :local,  :location, address_attributes: [:id, :place, :location, :lat, :lon])
     end
     
 end
