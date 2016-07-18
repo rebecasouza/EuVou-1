be91663684c7b24b8dda4031e8d97c6f7198af0a
rebeca souza <rebeca_souza85@hotmail.com>
2016-07-18 17:58:36 -0300

    fixing nested address for event error

--- app/models/event.rb
+++ app/models/event.rb
@@ -4,6 +4,7 @@ class Event < ApplicationRecord
   belongs_to :category
   has_many :comments, as: :commentable
   has_one :address
+  accepts_nested_attributes_for :address
   has_many :euvous
   has_many :users, through: :euvous
 end
