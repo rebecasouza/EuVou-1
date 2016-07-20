18101aa31bdcb6be592a9b21730806b70c668c7a
rebeca souza <rebeca_souza85@hotmail.com>
2016-07-20 19:37:48 -0300

    users serializers notification errors hotfix

--- app/serializers/user_serializer.rb
+++ app/serializers/user_serializer.rb
@@ -2,6 +2,6 @@ class UserSerializer < ActiveModel::Serializer
   attributes :id, :name, :email, :password, :admin
   has_many :events
   has_many :comments
-  has_many :notifications
+  #has_many :notifications
   has_one :address
 end
