5aa9e0971403cc4cb86375cba7a52b285467f935
rebeca souza <rebeca_souza85@hotmail.com>
2016-07-25 02:44:27 -0300

    adding default categories

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,31 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+# Adding default categories
+categories = [
+	"Negócios",
+	"Esportes",
+	"Saúde",
+	"Fitness",
+	"Festa",
+	"Educação",
+	"Hobbies",
+	"Artes",
+	"Música",
+	"Shows",
+	"Festival",
+	"Caridade",
+	"Gastronomia",
+	"Bebidas",
+	"Ciência e Tecnologia",
+	"Cinema",
+	"Espiritualidade",
+	"Moda",
+	"Viagem",
+	"Exposição"
+]
+
+categories.each do |category|
+	Category.create!(name: category)
+end
