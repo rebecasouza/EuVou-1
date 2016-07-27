26050764699f9e3399b9fc97b247218be785a775
rebeca souza <rebeca_souza85@hotmail.com>
2016-07-27 14:58:17 -0300

    making changes in users for authentication

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,8 @@
 class UsersController < ApplicationController
   before_action :set_user, only: [:show, :update, :destroy]
 
+  before_action :doorkeeper_authorize! 
+  
   # GET /users
   def index
     @users = User.all
@@ -11,17 +13,6 @@ class UsersController < ApplicationController
   # GET /users/1
   def show
     render json: @user
-  end
-
-  # POST /users
-  def create
-    @user = User.new(user_params)
-
-    if @user.save
-      render json: @user, status: :created, location: @user
-    else
-      render json: @user.errors, status: :unprocessable_entity
-    end
   end
 
   # PATCH/PUT /users/1
---
+++ db/migrate/20160725081719_edit_columsn_in_users.rb
@@ -0,0 +1,7 @@
+class EditColumsnInUsers < ActiveRecord::Migration[5.0]
+  def change
+  	change_table :users do |t|
+  		t.rename :password, :password_digest
+  	end
+  end
+end
